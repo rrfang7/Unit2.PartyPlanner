21681d4141519ac21552d8120d15ff8f4542fff7
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-03 19:41:15 -0400

    Update API .js

--- workshop.js
+++ workshop.js
@@ -1,1 +1,28 @@
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events`;
+
+const state = {
+    parties: [],
+};
+
+const partyList = document.querySelector("partyList");
+const addPartyForm = document.querySelector("#addParty");
+addPartyForm.addEventListener("submit", addParty);
+
+
+async function render() {
+    await getParties();
+    renderParties();
+}
+render();
+
+
+async function getParties() {
+    try {
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        state.parties = json;
+    }  catch(err) {
+        console.log(err)
+    }
+};
+
