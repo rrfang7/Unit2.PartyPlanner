8a3c125095e3bdb974ec14a247da935f6d048099
Ralph Fang <ralphrainierfang@yahoo.com>
2023-10-03 20:36:45 -0400

    Update html and JS

--- workshop.js
+++ workshop.js
@@ -4,6 +4,7 @@ const state = {
     parties: [],
 };
 
+// dom references
 const partyList = document.querySelector("partyList");
 const addPartyForm = document.querySelector("#addParty");
 addPartyForm.addEventListener("submit", addParty);
@@ -15,14 +16,67 @@ async function render() {
 }
 render();
 
-
+// update state with parties from API
 async function getParties() {
     try {
         const response = await fetch(API_URL);
         const json = await response.json();
         state.parties = json;
     }  catch(err) {
-        console.log(err)
+        console.error(err)
     }
 };
 
+// render artist from state 
+function renderParties() {
+    if (!state.parties.length) {
+        partyList.innerHTML = `<li>No parties found</li>`;
+        return
+    }
+
+    const partyItems = state.parties.map((party) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <span>Name: ${party.name}</span>
+        <span>Date: ${party.date}</span>
+        <span>Time: ${party.time}</span>
+        <span>Location: ${party.location}</span>
+        <span>Description: ${party.description}</span>
+        `;
+        return li;
+    });
+
+    partyList.replaceChildren(...partyItems);
+}
+
+async function addParty(event) {
+    event.preventDefault();
+
+    const partyName = document.getElementById("partyName").value;
+    const partyDate = document.getElementById("partyDate").value;
+    const partyTime = document.getElementById("partyTime").value;
+    const partyLocation = document.getElementById("partyLocation").value;
+    const partyDescription = document.getElementById("partyDescription").value;
+
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: partyName,
+                date: partyDate,
+                time: partyTime,
+                location: partyLocation,
+                description: partyDescription,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to create party");
+        }
+        
+        render();
+    } catch (error) {
+        console.error(error)
+    }
+}
